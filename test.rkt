0e930a210807d503ccc0e3ea38f05e627859b462
Bryan Coutts <b2coutts@uwaterloo.ca>
2014-08-23 23:11:38 -0400

    Added module to test board functionality
    TODO: make _ edges color all 5 tiles instead of middle 3

---
+++ test.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require "board.rkt" "util.rkt")
+
+(define (vtx a b c d e f) (list (cons a b) (cons c d) (cons e f)))
+(define (edg a b c d) (cons (cons a b) (cons c d)))
+
+(define blue (user 34))
+(define red (user 31))
+
+;; initial board
+(define b (create-board))
+
+(set-board-vertex-pair! b (vtx 0 0 0 2 1 1) blue 'settlement)
+(set-board-road-owner! b (edg 0 0 0 2) blue)
+(set-board-vertex-pair! b (vtx 0 -6 0 -4 1 -5) blue 'settlement)
+(set-board-road-owner! b (edg 0 -4 1 -5) blue)
+
+(set-board-vertex-pair! b (vtx -3 -1 -2 -2 -2 0) red 'settlement)
+(set-board-road-owner! b (edg -3 -1 -2 0) red)
+(set-board-vertex-pair! b (vtx -3 -1 -3 1 -2 0) red 'settlement)
+(set-board-road-owner! b (edg -3 1 -2 0) red)
+
+;; display board
+(display (board->string b))
