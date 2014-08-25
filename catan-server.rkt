c9cd4bc82645ec4393780ebe1ddc68fedbb4e37f
Bryan Coutts <b2coutts@uwaterloo.ca>
2014-08-25 19:29:45 -0400

    Added stub server implementation
    Actually does things with ports now. I still need a mechanism for creating the
    initial ports, and a final layer of connection of high level server logic.

--- catan-server.rkt
+++ catan-server.rkt
@@ -1,21 +1,36 @@
 #lang racket
 
-(define state (void))
+(define mutex (make-semaphore 1))
 
-(define (resource? x) (if (member x '(wood clay ore grain sheep)) #t #f))
-(define (item? x) (if (member x '(settlement city dev-card road)) #t #f))
+(define response? string?)
+
+;; begins interacting with a given user on the given input/output ports
+;; TODO: add a channel for when the game is won
+(define/contract (run-listener usr in out)
+  (-> input-port? output-port? void?)
+  (let loop []
+    (sync (read-line-evt in 'any))
+    (write (call-with-semaphore mutex (thunk (handle-action! usr evt))) out)
+    (loop)))
+
+;; TODO: replace these hardcoded connection details with something else
+;; TODO: actually hardcode these connection details
+(define state (create-state usr1 usr2))
+
+;; run listener threads
+(thread (thunk (run-listener usr1 in1 out1)))
+(thread (thunk (run-listener usr2 in2 out2)))
 
 (define/contract (handle-action! usr act)
   (-> user? action? response?)
   (match act
-    ;; TODO: trade
     [`(buy ,item) (buy-item! usr item)]
     [`(use ,card-num) (use-card! usr card-num)]
     [`(bank ,res-list ,target) (bank! usr res-list target)])
 
 (define/contract (buy-item! usr item)
   (-> user? item? response?)
-  (void))
+  
 
 (define/contract (use-card! usr card-num)
   (-> user? integer? response?)
