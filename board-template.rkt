3e4644323a806551d7dbcf2865d904d18b7bba9a
Bryan Coutts <b2coutts@uwaterloo.ca>
2014-09-05 19:57:34 -0400

    Implementing new fancy client
    It appears to perfectly display info; need to port user input stuff from the
    old client.

--- board-template.rkt
+++ board-template.rkt
@@ -4,90 +4,79 @@
 (provide board-template)
 
 (define board-template `(
-  (rep 20 sp) (hedge -1 5 0 4 0 6 1 5) nl
-  (rep 13 sp) (rep 5 (tp 0)) sp (edge #\/ -1 5 0 4) (label 0 4)
+  (rep 18 sp) (hedge -1 5 0 4 0 6 1 5) nl
+  (rep 11 sp) (rep 5 (tp 0)) sp (edge #\/ -1 5 0 4) (label 0 4)
     (edge #\\ 0 4 1 5) sp (rep 5 (tp 1)) nl
-  (rep 13 sp) (hedge -2 4 -1 3 -1 5 0 4) (edge #\/ -1 5 0 4) sp sp sp (num 0 4)
+  (rep 11 sp) (hedge -2 4 -1 3 -1 5 0 4) (edge #\/ -1 5 0 4) sp sp sp (num 0 4)
     sp sp (edge #\\ 0 4 1 5) (hedge 0 4 1 3 1 5 2 4) nl
-  (rep 12 sp) (edge #\/ -2 4 -1 3) (label -1 3)
+  (rep 10 sp) (edge #\/ -2 4 -1 3) (label -1 3)
     (edge #\\ -1 3 0 4) sp sp sp (res 0 4) sp sp sp (edge #\/ 0 4 1 3)
     (label 1 3) (edge #\\ 1 3 2 4) nl
-  (rep 6 sp) (hedge -3 3 -2 2 -2 4 -1 3) (edge #\/ -2 4 -1 3) sp sp sp
+  (rep 4 sp) (hedge -3 3 -2 2 -2 4 -1 3) (edge #\/ -2 4 -1 3) sp sp sp
     (num -1 3) sp sp (edge #\\ -1 3 0 4) (hedge -1 3 0 2 0 4 1 3)
     (edge #\/ 0 4 1 3) sp sp sp (num 1 3) sp sp (edge #\\ 1 3 2 4)
-    (hedge 1 3 2 2 2 4 3 3) (rep 10 sp) #\+ (rep 18 #\-) #\+ nl
-  (rep 3 sp) (tp 8) sp (edge #\/ -3 3 -2 2) (label -2 2)
+    (hedge 1 3 2 2 2 4 3 3) nl
+  (rep 1 sp) (tp 8) sp (edge #\/ -3 3 -2 2) (label -2 2)
     (edge #\\ -2 2 -1 3) sp sp sp (res -1 3) sp sp sp (edge #\/ -1 3 0 2)
     (label 0 2) (edge #\\ 0 2 1 3) sp sp sp (res 1 3) sp sp sp
-    (edge #\/ 1 3 2 2) (label 2 2) (edge #\\ 2 2 3 3) sp (tp 2)
-    (rep 7 sp) (str "| Legend:          |") nl
-  (rep 2 sp) (tp 8) (vertex #\space -3 1 -3 3 -2 2) (edge #\/ -3 3 -2 2) sp sp sp
+    (edge #\/ 1 3 2 2) (label 2 2) (edge #\\ 2 2 3 3) sp (tp 2) nl
+  (tp 8) (vertex #\space -3 1 -3 3 -2 2) (edge #\/ -3 3 -2 2) sp sp sp
     (num -2 2) sp sp (edge #\\ -2 2 -1 3) (hedge -2 2 -1 1 -1 3 0 2)
     (edge #\/ -1 3 0 2) sp sp sp (num 0 2) sp sp (edge #\\ 0 2 1 3)
     (hedge 0 2 1 1 1 3 2 2) (edge #\/ 1 3 2 2) sp sp sp (num 2 2) sp sp
-    (edge #\\ 2 2 3 3) (vertex #\space 2 2 3 1 3 3) (tp 2) (rep 6 sp)
-    (str "+---+--------------+") nl
-  (rep 4 sp) (edge #\\ -3 1 -2 2) sp sp sp (res -2 2) sp sp sp
+    (edge #\\ 2 2 3 3) (vertex #\space 2 2 3 1 3 3) (tp 2) nl
+  (rep 2 sp) (edge #\\ -3 1 -2 2) sp sp sp (res -2 2) sp sp sp
     (edge #\/ -2 2 -1 1) (label -1 1) (edge #\\ -1 1 0 2) sp sp sp
     (res 0 2) sp sp sp (edge #\/ 0 2 1 1) (label 1 1)
-    (edge #\\ 1 1 2 2) sp sp sp (res 2 2) sp sp sp (edge #\/ 2 2 3 1) (rep 8 sp)
-    #\| sp (sty (40 92 #f #f) #\s) (str " | sheep        |") nl
-  (rep 5 sp) (edge #\\ -3 1 -2 2) (hedge -3 1 -2 0 -2 2 -1 1)
+    (edge #\\ 1 1 2 2) sp sp sp (res 2 2) sp sp sp (edge #\/ 2 2 3 1) nl
+  (rep 3 sp) (edge #\\ -3 1 -2 2) (hedge -3 1 -2 0 -2 2 -1 1)
     (edge #\/ -2 2 -1 1) sp sp sp (num -1 1) sp sp (edge #\\ -1 1 0 2)
     (hedge -1 1 0 0 0 2 1 1) (edge #\/ 0 2 1 1) sp sp sp (num 1 1) sp sp
-    (edge #\\ 1 1 2 2) (hedge 1 1 2 0 2 2 3 1) (edge #\/ 2 2 3 1) (rep 9 sp)
-    #\| sp (sty (40 31 #f #f) #\c) (str " | clay         |") nl
-  (rep 5 sp) (edge #\/ -3 1 -2 0) (label -2 0) (edge #\\ -2 0 -1 1)
+    (edge #\\ 1 1 2 2) (hedge 1 1 2 0 2 2 3 1) (edge #\/ 2 2 3 1) nl
+  (rep 3 sp) (edge #\/ -3 1 -2 0) (label -2 0) (edge #\\ -2 0 -1 1)
     sp sp sp (res -1 1) sp sp sp (edge #\/ -1 1 0 0) (label 0 0)
     (edge #\\ 0 0 1 1) sp sp sp (res 1 1) sp sp sp (edge #\/ 1 1 2 0)
-    (label 2 0) (edge #\\ 2 0 3 1) (rep 9 sp) #\| sp (sty (40 33 #f #f) #\g)
-    (str " | grain        |") nl
-  (rep 3 sp) (vertex #\space -3 -1 -3 1 -2 0) (edge #\/ -3 1 -2 0) sp sp sp
+    (label 2 0) (edge #\\ 2 0 3 1) nl
+  (rep 1 sp) (vertex #\space -3 -1 -3 1 -2 0) (edge #\/ -3 1 -2 0) sp sp sp
     (num -2 0) sp sp (edge #\\ -2 0 -1 1) (hedge -2 0 -1 -1 -1 1 0 0)
     (edge #\/ -1 1 0 0) sp sp sp (num 0 0) sp sp (edge #\\ 0 0 1 1)
     (hedge 0 0 1 -1 1 1 2 0) (edge #\/ 1 1 2 0) sp sp sp (num 2 0) sp sp
-    (edge #\\ 2 0 3 1) (vertex #\space 2 0 3 -1 3 1) (rep 7 sp)
-    #\| sp (sty (40 32 #f #f) #\w) (str " | wood         |") nl
-  (rep 2 sp) (tp 7) sp (edge #\\ -3 -1 -2 0) sp sp sp (res -2 0) sp sp sp
+    (edge #\\ 2 0 3 1) (vertex #\space 2 0 3 -1 3 1) nl
+  (tp 7) sp (edge #\\ -3 -1 -2 0) sp sp sp (res -2 0) sp sp sp
     (edge #\/ -2 0 -1 -1) (label -1 -1) (edge #\\ -1 -1 0 0) sp sp
     sp (res 0 0) sp sp sp (edge #\/ 0 0 1 -1) (label 1 -1)
-    (edge #\\ 1 -1 2 0) sp sp sp (res 2 0) sp sp sp (edge #\/ 2 0 3 -1) sp (tp 3)
-    (rep 6 sp) #\| sp (sty (40 90 #f #f) #\o) (str " | ore          |") nl
-  (rep 3 sp) (tp 7) sp (edge #\\ -3 -1 -2 0) (hedge -3 -1 -2 -2 -2 0 -1 -1)
+    (edge #\\ 1 -1 2 0) sp sp sp (res 2 0) sp sp sp (edge #\/ 2 0 3 -1) sp
+    (tp 3) nl
+  (rep 1 sp) (tp 7) sp (edge #\\ -3 -1 -2 0) (hedge -3 -1 -2 -2 -2 0 -1 -1)
     (edge #\/ -2 0 -1 -1) sp sp sp (num -1 -1) sp sp (edge #\\ -1 -1 0 0)
     (hedge -1 -1 0 -2 0 0 1 -1) (edge #\/ 0 0 1 -1) sp sp sp (num 1 -1) sp sp
     (edge #\\ 1 -1 2 0) (hedge 1 -1 2 -2 2 0 3 -1) (edge #\/ 2 0 3 -1) sp (tp 3)
-    (rep 7 sp) #\| sp (sty (40 37 #f #f) #\d) (str " | desert       |") nl
-  (rep 5 sp) (edge #\/ -3 -1 -2 -2) (label -2 -2)
+    nl
+  (rep 3 sp) (edge #\/ -3 -1 -2 -2) (label -2 -2)
     (edge #\\ -2 -2 -1 -1) sp sp sp (res -1 -1) sp sp sp (edge #\/ -1 -1 0 -2)
     (label 0 -2) (edge #\\ 0 -2 1 -1) sp sp sp (res 1 -1) sp sp sp
-    (edge #\/ 1 -1 2 -2) (label 2 -2) (edge #\\ 2 -2 3 -1)
-    (rep 9 sp) #\| sp (sty (40 31 #f #f) #\T) (str " | thief        |") nl
-  (rep 3 sp) (vertex #\space -3 -3 -3 -1 -2 -2) (edge #\/ -3 -1 -2 -2) sp sp sp
+    (edge #\/ 1 -1 2 -2) (label 2 -2) (edge #\\ 2 -2 3 -1) nl
+  (rep 1 sp) (vertex #\space -3 -3 -3 -1 -2 -2) (edge #\/ -3 -1 -2 -2) sp sp sp
     (num -2 -2) sp sp (edge #\\ -2 -2 -1 -1) (hedge -2 -2 -1 -3 -1 -1 0 -2)
     (edge #\/ -1 -1 0 -2) sp sp sp (num 0 -2) sp sp (edge #\\ 0 -2 1 -1)
     (hedge 0 -2 1 -3 1 -1 2 -2) (edge #\/ 1 -1 2 -2) sp sp sp (num 2 -2) sp sp
-    (edge #\\ 2 -2 3 -1) (vertex #\space 2 -2 3 -3 3 -1) (rep 7 sp)
-    (str "| ? | 3:1 post     |") nl
-  (rep 4 sp) (edge #\\ -3 -3 -2 -2) sp sp sp (res -2 -2) sp sp sp
+    (edge #\\ 2 -2 3 -1) (vertex #\space 2 -2 3 -3 3 -1) nl
+  (rep 2 sp) (edge #\\ -3 -3 -2 -2) sp sp sp (res -2 -2) sp sp sp
     (edge #\/ -2 -2 -1 -3) (label -1 -3) (edge #\\ -1 -3 0 -2)
     sp sp sp (res 0 -2) sp sp sp (edge #\/ 0 -2 1 -3) (label 1 -3)
-    (edge #\\ 1 -3 2 -2) sp sp sp (res 2 -2) sp sp sp (edge #\/ 2 -2 3 -3)
-    (rep 8 sp) (str "| S | settlement   |") nl
-  (rep 5 sp) (edge #\\ -3 -3 -2 -2) (hedge -3 -3 -2 -4 -2 -2 -1 -3)
+    (edge #\\ 1 -3 2 -2) sp sp sp (res 2 -2) sp sp sp (edge #\/ 2 -2 3 -3) nl
+  (rep 3 sp) (edge #\\ -3 -3 -2 -2) (hedge -3 -3 -2 -4 -2 -2 -1 -3)
     (edge #\/ -2 -2 -1 -3) sp sp sp (num -1 -3) sp sp (edge #\\ -1 -3 0 -2)
     (hedge -1 -3 0 -4 0 -2 1 -3) (edge #\/ 0 -2 1 -3) sp sp sp (num 1 -3) sp sp
-    (edge #\\ 1 -3 2 -2) (hedge 1 -3 2 -4 2 -2 3 -3) (edge #\/ 2 -2 3 -3)
-    (rep 9 sp) (str "| C | city         |") nl
-  (rep 9 sp) (tp 6) sp (edge #\\ -2 -4 -1 -3) sp sp sp (res -1 -3) sp sp sp
+    (edge #\\ 1 -3 2 -2) (hedge 1 -3 2 -4 2 -2 3 -3) (edge #\/ 2 -2 3 -3) nl
+  (rep 7 sp) (tp 6) sp (edge #\\ -2 -4 -1 -3) sp sp sp (res -1 -3) sp sp sp
     (edge #\/ -1 -3 0 -4) (label 0 -4) (edge #\\ 0 -4 1 -3) sp sp sp
-    (res 1 -3) sp sp sp (edge #\/ 1 -3 2 -4) sp (tp 4) (rep 13 sp)
-    (str "+------------------+") nl
-  (rep 10 sp) (tp 6) sp (edge #\\ -2 -4 -1 -3) (hedge -2 -4 -1 -5 -1 -3 0 -4)
+    (res 1 -3) sp sp sp (edge #\/ 1 -3 2 -4) sp (tp 4) nl
+  (rep 8 sp) (tp 6) sp (edge #\\ -2 -4 -1 -3) (hedge -2 -4 -1 -5 -1 -3 0 -4)
     (edge #\/ -1 -3 0 -4) sp sp sp (num 0 -4) sp sp (edge #\\ 0 -4 1 -3)
     (hedge 0 -4 1 -5 1 -3 2 -4) (edge #\/ 1 -3 2 -4) sp (tp 4) nl
-  (rep 18 sp) (edge #\\ -1 -5 0 -4) sp sp sp (res 0 -4) sp sp sp
+  (rep 16 sp) (edge #\\ -1 -5 0 -4) sp sp sp (res 0 -4) sp sp sp
     (edge #\/ 0 -4 1 -5) nl
-  (rep 19 sp) (edge #\\ -1 -5 0 -4) (hedge -1 -5 0 -6 0 -4 1 -5)
+  (rep 17 sp) (edge #\\ -1 -5 0 -4) (hedge -1 -5 0 -6 0 -4 1 -5)
     (edge #\/ 0 -4 1 -5) nl
-  (rep 20 sp) (rep 5 (tp 5)) nl))
+  (rep 18 sp) (rep 5 (tp 5)) nl))
--- client.rkt
+++ client.rkt
@@ -1,14 +1,12 @@
-;; client for connecting to the soa server
+;; TODO deal with *really* long commands (for some reason)
 #lang racket
 
-(require racket/contract "basic.rkt" "adv.rkt" "data.rkt" "constants.rkt"
-         "help.rkt")
+(require (planet neil/charterm) racket/string racket/list)
 
-;; macro for writing something over TCP
-(define-syntax-rule (send msg out) (fprintf out "~s\n" msg))
+(define (interp x) (with-input-from-string x (thunk (read))))
 
-(display (style->string '(40 37 #f #f)))
-
+;; ---------------------------- INITIAL SETUP CODE ----------------------------
+;; get connection/name info
 (printf "Welcome to asciitan!\n")
 (printf "What is the hostname or IP of the server?\n")
 (define host (read-line))
@@ -23,124 +21,159 @@
 (define name (read-line))
 (printf "Connecting to server...\n")
 
-(send name srv-out)
+;; establish connection to listener
+(fprintf srv-out "~s\n" name)
 (define listener-port (read srv-in))
-
-(define-values (listen-in listen-out) (tcp-connect host listener-port))
-(file-stream-buffer-mode listen-out 'line)
-
+(define-values (game-in game-out) (tcp-connect host listener-port))
+(file-stream-buffer-mode game-out 'line)
 (printf "Connection established; waiting for other users to connect.\n")
 
-(define (interp x) (with-input-from-string x (thunk (read))))
+;; wait for initial state data
+(match-define `(raw ,init-board)
+  (interp (sync (read-line-evt game-in 'any))))
 
-;; parse a line of user input
-(define/contract (parse str)
-  (-> string? (listof string?))
-  (define lst (string->list str))
+;; Setup UI
+(open-charterm)
+(define-values (width height) (charterm-screen-size))
+
+;; ---------------------- UI MANIPULATION CODE ----------------------
+;; (reversed) bottom line of user's text input
+(define input '())
+
+;; lines of console text
+(define clines '())
+
+;; get the index of the latest #\space in a string
+(define (last-space str [idx (- (string-length str) 1)])
   (cond
-    [(empty? lst) '()]
-    [else (define-values (x xs) (splitf-at lst (not/c (or/c #\tab #\space))))
-          (cons (list->string x)
-                (parse (list->string (dropf xs (or/c #\tab #\space)))))]))
+    [(< idx 0) #f]
+    [(char=? (string-ref str idx) #\space) idx]
+    [else (last-space str (sub1 idx))]))
 
-(define ss string->symbol) ;; to make my life easier
+;; wrap a single string to the specified number of characters
+(define (wrap-str str len)
+  (cond
+    [(<= (string-length str) len)
+      (list (string-append str (make-string (- len (string-length str))
+                                            #\space)))]
+    [else (match (last-space (substring str 0 len))
+      [#f (cons (substring str 0 len) (wrap-str (substring str len) len))]
+      [idx (cons (substring str 0 idx)
+                 (wrap-str (substring str (add1 idx)) len))])]))
 
-(define (repl)
-  (define evt (sync (wrap-evt (read-line-evt (current-input-port) 'any)
-                              (curry cons 'user))
-                    (wrap-evt (read-line-evt listen-in 'any)
-                              (curry cons 'server))))
-  (match evt
-    [(cons _ (? eof-object?)) (printf "EOF encountered. Exiting.\n")]
-    [(cons 'user msg)
-      (define req (match (parse msg)
-        ;; prompt responses
-        [`("move" ,cstr) (define cell (label->cell (ss cstr)))
-          (cond [cell `(respond move-thief ,cell)]
-                [else (printf "! invalid cell: ~a\n" cstr)])]
-        [`("take" ,res) (cond
-          [(resource? (ss res)) `(respond monopoly ,(ss res))]
-          [else (printf "! invalid resource: ~a\n" res)])]
-        [`("choose" ,res1 ,res2) (cond
-          [(not (resource? (ss res1))) (printf "! invalid resource: ~a\n" res1)]
-          [(not (resource? (ss res2))) (printf "! invalid resource: ~a\n" res2)]
-          [else `(respond year-of-plenty ,(cons (ss res1) (ss res2)))])]
-        [`("build" ,e1 ,e2) (match (cons (string->edge e1) (string->edge e2))
-          [(cons #f _) (printf "! invalid edge: ~a\n" e1)]
-          [(cons _ #f) (printf "! invalid edge: ~a\n" e2)]
-          [edgs `(respond road-building ,edgs)])]
-        [(cons "discard" rlist)
-          (match (filter-not (compose resource? ss) rlist)
-            [(cons str _) (printf "! invalid resource: ~a\n" str)]
-            [_ `(respond discard-resources ,(map ss rlist))])]
-        ;; TODO: validate the username
-        [`("steal" ,usr) `(respond pick-target ,usr)]
-        [`("place" "road" ,estr) (match (string->edge estr)
-          [#f (printf "! invalid edge: ~a\n" estr)]
-          [edg `(respond init-road ,edg)])]
-        [`("place" "settlement" ,vstr) (match (string->vertex vstr)
-          [#f (printf "! invalid vertex: ~a\n" vstr)]
-          [vtx `(respond init-settlement ,vtx)])]
-        [(cons "offer" (cons target ress)) (cond
-          [(not (member? "for" ress)) (printf "Usage: ~a\n"
-                                              (car (help-cmd '(offer))))]
-          [else
-            (match-define-values (give (cons "for" get))
-                                 (splitf-at ress (not/c "for")))
-            (match (filter (not/c (compose resource? ss)) (append give get))
-              [(cons str _) (printf "! invalid resource: ~a\n" str)]
-              ['() `(offer ,target ,(map ss give) ,(map ss get))])])]
-        ['("accept") '(respond trade accept)]
-        ['("decline") '(respond trade decline)]
+;; wraps a console message to fit in the console
+(define (wrap-msg pad str)
+  (define len (string-length pad))
+  (match-define (cons x xs) (wrap-str str (- width len 43)))
+  (cons (string-append pad x)
+        (map (curry string-append (list->string (make-list len #\space))) xs)))
 
-        [(cons "help" args) (match (help-cmd (map ss args))
-          [(cons usage details) (printf "Usage: ~a\n~a\n" usage details)]
-          [info (printf "~a\n" info)])]
-        [`("buy" "dev-card") `(buy dev-card)]
-        [`("buy" "road" ,edg) (cond
-          [(not (string->edge edg))
-            (printf "! invalid edge: ~a\n" edg)]
-          [else `(buy road ,(string->edge edg))])]
-        [`("buy" ,item ,vtx) (cond
-          [(not (building? (ss item)))
-            (printf "! invalid usage of buy\n")]
-          [(not (string->vertex vtx))
-            (printf "! invalid vertex: ~a\n" vtx)]
-          [else `(buy ,(ss item) ,(string->vertex vtx))])]
-        [`("use" ,card) (match (ss card)
-          [(? dev-card? dc) `(use ,dc)]
-          [_ (printf "! not a dev card: ~a\n" card)])]
-        [(cons "bank" (cons target lst))
-          (define err (ormap (lambda (res) (cond
-                              [(resource? (ss res)) #f]
-                              [else (printf "! invalid resource: ~a\n" res)]))
-                             (cons target lst)))
-          (if err (void)
-            `(bank ,(map ss lst) ,(ss target)))]
-        [`("end") '(end)]
-        [`("show" ,thing) (cond
-          [(showable? (ss thing)) `(show ,(ss thing))]
-          [else (printf "! invalid thing to show: ~a\n" thing)])]
-        [(cons "say" _) `(say ,(if (< (string-length msg) 4)
-                                   "" (substring msg 4)))]
-        [(cons cmd args) (cond
-          [(or (command? (ss cmd)) (member? (ss cmd) client-commands))
-            (printf "Usage: ~a\n" (car (help-cmd (list (ss cmd)))))]
-          [else (printf "! invalid command: ~a\n" cmd)])]
-        [_ (printf "! badly formatted request: ~a\n" msg)]))
-      (when (not (void? req))
-        (fprintf listen-out "~s\n" req))
-      (when (not (equal? req 'game-over))
-        (repl))]
-    [(cons 'server msg)
-      (define continue
-        (match (interp msg)
-          [`(broadcast ,msg) (printf "* ~a\n" msg)]
-          [`(message ,msg) (printf "? ~a\n" msg)]
-          [`(raw ,msg) (display msg)]
-          [`(say ,name ,msg) (printf "~a: ~a\n" name msg)]
-          [`(prompt ,type ,msg) (printf "> ~a\n" msg)]
-          [`(game-over) #f]
-          [x (printf "ERROR: unknown command from server: ~a\n" x)]))
-      (when continue (repl))]))
-(repl)
+;; refreshes/updates the console
+(define (refresh-console! [ind 0] [line (- height 1)])
+  (when (and (> line 1) (< ind (length clines)))
+    (define strs (wrap-msg (car (list-ref clines ind))
+                           (cdr (list-ref clines ind))))
+    (map (lambda (i)
+          (charterm-cursor 44 (- line i))
+          (charterm-display (list-ref strs (- (length strs) i 1))))
+         (range 0 (min (length strs) (- line 1))))
+    (refresh-console! (add1 ind) (- line (length strs)))))
+
+;; add a new line to the console, and refresh it
+;; TODO: deal with wrapping properly
+(define/contract (console! pad str)
+  (-> string? string? void?)
+  (reset-colors)
+  (set! clines (cons (cons pad str) clines))
+  (refresh-console!)
+  (cursor-input))
+
+;; update the board state with a new board from the server
+;; TODO: handle other state updates from server
+(define/contract (update-board! lines)
+  (-> (listof string?) void?)
+  (reset-colors)
+  (map (lambda (ind)
+        (charterm-cursor 1 (+ ind 2))
+        (charterm-display (list-ref lines ind)))
+       (range 0 (length lines)))
+  (cursor-input))
+
+;; sets the text in the status bar
+;; TODO: deal with really long statuses?
+(define/contract (set-status str)
+  (-> string? void?)
+  (charterm-cursor 1 1)
+  (charterm-display (format "~a[30;47m~a~a" (integer->char #x1b) str
+                      (make-string (- width (string-length str)) #\space)))
+  (cursor-input))
+
+;; draws the middle | separator
+(define/contract (draw-separator)
+  (-> void?)
+  (reset-colors)
+  (map (lambda (row)
+        (charterm-cursor 42 row)
+        (charterm-display "|"))
+       (range 2 height))
+  (cursor-input))
+
+;; moves the charterm console to the proper spot in the input bar
+(define/contract (cursor-input)
+  (-> void?)
+  (charterm-cursor (+ (length input) 3) height)
+  (charterm-display (format "~a[37;44m" (integer->char #x1b))))
+
+;; clears the input prompt line
+(define/contract (clear-prompt)
+  (-> void?)
+  (charterm-cursor 1 height)
+  (charterm-display (format "~a[37;44m>~a" (integer->char #x1b)
+                      (make-string (- width 1) #\space)))
+  (charterm-cursor 3 height))
+
+;; sets the terminal colours back to default
+(define/contract (reset-colors)
+  (-> void?)
+  (charterm-display (format "~a[37;40m" (integer->char #x1b))))
+
+;; ------------------------ END OF UI MANIPULATION CODE ------------------------
+;; TODO: implement this properly
+(define (user-cmd str)
+  (when (string=? str "really-quit") (exit))
+  (console! "$ " str))
+
+(charterm-clear-screen)
+(clear-prompt)
+(draw-separator)
+(set-status "foo")
+(update-board! (string-split init-board "\n"))
+
+(define (loop)
+  (match (sync (wrap-evt (current-charterm) (curry cons 'user))
+               (wrap-evt (read-line-evt game-in 'any) (curry cons 'server)))
+    [(cons 'user _) (match (charterm-read-key)
+      ['return
+        (unless (empty? input)
+          (user-cmd (list->string (reverse input)))
+          (set! input '())
+          (clear-prompt))]
+      ['backspace
+        (unless (empty? input)
+          (charterm-cursor (+ (length input) 2) height)
+          (charterm-display " ")
+          (charterm-cursor (+ (length input) 2) height)
+          (set! input (rest input)))]
+      [ch (charterm-display (~a ch))
+          (set! input (cons ch input))])]
+    [(cons 'server msg) (match (interp msg)
+      [`(broadcast ,msg) (console! "* " msg)]
+      [`(message ,msg) (console! "? " msg)]
+      [`(prompt ,_ ,msg) (console! "> " msg)]
+      [`(say ,name ,msg) (console! (format "~a: " name) msg)]
+      [`(raw ,brd) (update-board! (string-split brd "\n"))] ;; TODO: raw => update
+      [`(raw ,_) (error "client received raw")]
+      ;; TODO: return terminal to normal?
+      [`(game-over) (exit)])])
+  (loop))
+(loop)
